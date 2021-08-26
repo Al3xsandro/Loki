d7f5ad768aec33c0a883c285fa6ba313a0338ee1
Alex <alexs123655@gmail.com>
2021-08-26 08:59:11 -0300

    initial commit

--- src/components/featured/index.tsx
+++ src/components/featured/index.tsx
@@ -3,6 +3,15 @@ import { Container } from "./style";
 export function Featured() {
     return (
         <Container>
+            <div className="featured">
+                <h2 className="featured_flag">ESTÁ DISPONÍVEL AGORA!</h2>
+
+                <h2 className="featured_title">LOKI</h2>
+
+                <div className="featured_section">
+                    <div className="featured_content"></div>
+                </div>
+            </div>
         </Container>
     );
 };
--- src/components/featured/style.ts
+++ src/components/featured/style.ts
@@ -6,6 +6,35 @@ export const Container = styled.div`
 
     height: 100%;
     width: 100%;
-    
-    background: url('/images/background.png') no-repeat;
+
+    background: url('/images/background.png') no-repeat center;
+    background-size: 100% 100%;
+
+    box-shadow: inset 0px 0px 400px 510px rgba(0, 0, 0, .7);
+
+    overflow: hidden;
+
+    .featured {
+        display: flex;
+        align-items: center;
+        flex-direction: column;
+
+        padding: 1rem;
+
+        .featured_flag {
+            font-family: 'B612', sans-serif;
+            font-weight: 700;
+            font-size: 1.4rem;
+            line-height: 3rem;
+            color: var(--white);
+        }
+
+        .featured_title {
+            font-family: 'B612', sans-serif;
+            font-weight: 700;
+            line-height: 7rem;
+            font-size: 5.4rem;
+            color: var(--white);
+        }
+    }
 `;
