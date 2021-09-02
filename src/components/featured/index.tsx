64ba699e2649e690f6993adda7156a4b69b16ce3
Alex <alexs123655@gmail.com>
2021-09-02 08:23:46 -0300

    feat: new section for movie details

--- src/components/featured/index.tsx
+++ src/components/featured/index.tsx
@@ -9,7 +9,17 @@ export function Featured() {
                 <h2 className="featured_title">LOKI</h2>
 
                 <div className="featured_section">
-                    <div className="featured_content"></div>
+                    <img className="feature_logo" src="/images/capa.png" alt="loki"/>
+                    <div className="featured_content">
+                        <div className="featured_details"></div>
+                        <p className="featured_description">
+                            Em “Loki” da Marvel Studios, o vilão mercurial 
+                            Loki (Tom Hiddleston) retoma seu papel
+                            como o Deus do Mal em uma nova série que ocorre
+                            após os eventos de “Vingadores: Endgame”.
+                        </p>
+                        <div className="featured_buttons"></div>
+                    </div>
                 </div>
             </div>
         </Container>
--- src/components/featured/style.ts
+++ src/components/featured/style.ts
@@ -36,10 +36,42 @@ export const Container = styled.div`
             font-size: 5.4rem;
             color: var(--white);
         }
+
+        .featured_section {
+            display: flex;
+            justify-content: space-between;
+
+            margin: 3.5rem;
+
+            max-width: 60rem;
+
+            .featured_content {
+                display: flex;
+                padding: 1rem 2.75rem;
+
+                .featured_description {
+                    font-family: 'B612', sans-serif;
+                    color: var(--white);
+                }
+            }
+        };
     }
 
     @media(max-width: 768px){
         background: url('/images/background-mobile.png') no-repeat center;
         background-size: 100% 100%;
+    
+        .featured_section {
+            display: flex;
+            justify-content: center;
+            flex-direction: column;
+
+            .feature_logo {
+                display: flex;
+                justify-content: center;
+
+                margin: 0 auto;
+            }
+        }
     }
 `;
