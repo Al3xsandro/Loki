12d709fbf38916250e87f97fee6660175d97c964
Alex <alexs123655@gmail.com>
2021-09-19 17:26:03 -0300

    feat: player context

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -2,14 +2,22 @@ import { Container } from './style';
 
 import { HeaderMenu } from '../../components/header';
 import { Featured } from '../../components/featured';
+import { Player } from '../../components/player';
+
+import { usePlayer } from '../../hooks/usePlayer';
 
 export function Home() {
+    const { showPlayer } = usePlayer();
+
     return (
         <>
             <HeaderMenu/>
             
             <Container>
                 <Featured/>
+                {
+                    showPlayer && <Player/>
+                };
             </Container>
         </>
     );
